ce4835f8ee908cb798d5627225f000ede81357dd
David Anderson <fapulito@gmail.com>
2024-02-11 20:33:00 -0800

    Add space for output viewing

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
 
   return (
     <>
-      <button onClick={insert}>Test Dapp - insert document</button>
+      <button onClick={insert}>Test Dapp - insert document </button>
       {record !== undefined && <span>Key: {record.key}</span>}
     </>
   );
